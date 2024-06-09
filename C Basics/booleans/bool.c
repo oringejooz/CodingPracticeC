544dcd33689996ee818d3dc5250a4871c456c09f
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-06-10 02:40:44 +0530

    new file:   C Basics/booleans/bool.c 	new file:   C Basics/booleans/eighteen.c 	new file:   C Basics/if_else/checkingnum.c

---
+++ C Basics/booleans/bool.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// the bool type is not a built-in data type,
+//import the following header file
+
+#include <stdbool.h>
+
+
+int main()
+{
+    bool v1 = true;
+    bool v2 = false;
+    printf("%d",9>2);//returns 1 cuz true
+    printf("%d", v1==v2);
+    return 0;
+}
---
+++ C Basics/booleans/eighteen.c
@@ -0,0 +1,16 @@
+//WAP to find if a person is old enough to vote
+
+#include <stdio.h>
+int main()
+{
+int age;
+scanf("%d",&age);
+if(age>=18){
+    printf("VOTE VOTE VOTE");
+}
+else{
+    printf("NO VOTING FOR YOU KID");
+}
+
+return 0;
+}
